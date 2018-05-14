db03dad576b172df9385c52c4d467f20921c24c6
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-05-14 09:24:04 +0000

    - サンプル調整。

--- example/person/findAccounts.js
+++ example/person/findAccounts.js
@@ -4,22 +4,69 @@
  */
 
 const moment = require('moment');
+const open = require('open');
+const readline = require('readline');
 const util = require('util');
-const pecorinoapi = require('../../lib/');
-
-const oauth2client = new pecorinoapi.auth.OAuth2({
-    domain: process.env.TEST_AUTHORIZE_SERVER_DOMAIN
-});
-oauth2client.setCredentials({
-    access_token: process.env.TEST_ACCESS_TOKEN
-});
-
-const personService = new pecorinoapi.service.Person({
-    endpoint: process.env.KWSKFS_API_ENDPOINT,
-    auth: oauth2client
-});
+const kwskfsapi = require('../../lib/');
 
 async function main() {
+    const scopes = [];
+
+    const auth = new kwskfsapi.auth.OAuth2({
+        domain: process.env.TEST_AUTHORIZE_SERVER_DOMAIN,
+        clientId: process.env.TEST_CLIENT_ID_OAUTH2,
+        clientSecret: process.env.TEST_CLIENT_SECRET_OAUTH2,
+        redirectUri: 'https://localhost/signIn',
+        logoutUri: 'https://localhost/signOut'
+    });
+
+    const state = '12345';
+    const codeVerifier = '12345';
+
+    const authUrl = auth.generateAuthUrl({
+        scopes: scopes,
+        state: state,
+        codeVerifier: codeVerifier
+    });
+    console.log('authUrl:', authUrl);
+
+    open(authUrl);
+
+    await new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question('enter authorization code:\n', async (code) => {
+            rl.question('enter state:\n', async (givenState) => {
+                if (givenState !== state) {
+                    reject(new Error('state not matched'));
+
+                    return;
+                }
+
+                let credentials = await auth.getToken(code, codeVerifier);
+                console.log('credentials published', credentials);
+
+                auth.setCredentials(credentials);
+
+                credentials = await auth.refreshAccessToken();
+                console.log('credentials refreshed', credentials);
+
+                rl.close();
+                resolve();
+            });
+        });
+    });
+
+    const logoutUrl = auth.generateLogoutUrl();
+    console.log('logoutUrl:', logoutUrl);
+
+    const personService = new kwskfsapi.service.Person({
+        endpoint: process.env.KWSKFS_API_ENDPOINT,
+        auth: auth
+    });
     const accounts = await personService.findAccounts({
         personId: 'me'
     });
